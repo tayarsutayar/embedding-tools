8af0a44e4dbb0decac11c0cb17ab2549a1b74490
root <root@srv413003.hstgr.cloud>
2023-08-30 13:51:25 +0000

    remove edge function

--- pages/api/query_wa.ts
+++ pages/api/query_wa.ts
@@ -4,8 +4,6 @@ import { PineconeStore } from "langchain/vectorstores/pinecone"
 import { initPinecone } from "@/config/pinecone"
 import { makePdfChain } from "@/lib/chain"
 
-export const runtime = 'edge'
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
